1f5dd21c3a7ee247d17d67a6c08e7c33db8d7c51
Julia <yulia.bobko@mail.ru>
2023-01-16 15:15:34 +0300

    optimization debounce

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,13 +7,14 @@ import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Pagination from "../Pagination";
 import { SearchContext } from '../App';
-
 import { useSelector, useDispatch } from 'react-redux';
 import { setCategoryId, setCurrentPage } from '../redux/slises/filterSlice';
 
 
+
+
 const Home = () => {
-    const {categoryId, sortType, currentPage}= useSelector(state => state.filter);
+    const { categoryId, sortType, currentPage } = useSelector(state => state.filter);
     const dispatch = useDispatch();
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
@@ -23,12 +24,6 @@ const Home = () => {
 
     React.useEffect(() => {
         setIsLoading(true);
-        // fetch(`https://63b403f1ea89e3e3db53cf0c.mockapi.io/items?limit=4&page=${currentPage}${category}&sortBy=${sortType.sort}&order=asc${search}`
-        // ).then(res => res.json()).then(res => {
-        //             setItems(res);
-        //             setIsLoading(false);
-        //         }
-        //     )
         axios.get(`https://63b403f1ea89e3e3db53cf0c.mockapi.io/items?limit=4&page=${currentPage}${category}&sortBy=${sortType.sort}&order=asc${search}`
         ).then(res => {
             setItems(res.data);
@@ -37,20 +32,8 @@ const Home = () => {
         window.scrollTo(0, 0);
     }, [categoryId, sortType, searchValue, currentPage]);
 
-    const handlePageClick = (event) => {
-        const newOffset = (event.selected * 3) % items.length;
-        console.log(
-            `User requested page number ${event.selected}, which is offset ${newOffset}`
-        );
-        //setItemOffset(newOffset);
-    };
-
     const onChangeCategory = (i) => {
         dispatch(setCategoryId(i));
-    }
-
-    const setPage = (e)=>{
-
     }
 
     return (<>
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,22 +4,39 @@ import s from '../Search/Search.module.scss';
 import clear from '../../assets/img/icons/clear.svg';
 import search from '../../assets/img/icons/search.svg';
 import { SearchContext } from "../../App";
+import debounce from "lodash.debounce";
+
 
 const Search = () => {
-    const { searchValue, setSearchValue } = React.useContext(SearchContext);
+    const [value, setValue]= React.useState('');
+    const {setSearchValue } = React.useContext(SearchContext);
     const inputRef = React.useRef();
 
+    const testDebounce = React.useCallback(debounce(() => console.log('hello'), 1000), []);
+
+
     const onClickClear = () => {
-        setSearchValue('');
-        console.log(inputRef);
+        setSearchValue(''); 
+        setValue('');
         inputRef.current.focus();
+    }
+
+    const onChangeSearchValue = (str) => {
+        setSearchValue(str);
+        testDebounce();
+    }
+
+    const onChangeInput = event =>{
+        setValue(event.target.value);
+        onChangeSearchValue(event.target.value);
+
     }
 
     return (
         <div className={s.root}>
             <img className={s.search} src={search} />
-            <input ref={inputRef} onChange={(e) => { setSearchValue(e.target.value) }} className={s.input} placeholder="Поиск пиццы..." value={searchValue} />
-            {searchValue && <img onClick={()=>onClickClear() } className={s.clear} src={clear} />}
+            <input ref={inputRef} onChange={onChangeInput} className={s.input} placeholder="Поиск пиццы..." value={value} />
+            {value && <img onClick={() => onClickClear()} className={s.clear} src={clear} />}
         </div>
     )
 }
