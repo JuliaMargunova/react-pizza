7c7a75f92c29980c1253a65ee752e5a48867964f
Julia <yulia.bobko@mail.ru>
2023-01-30 11:13:21 +0300

    added react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import Home from './Pages/Home';
 import NotFound from './Pages/NotFound';
-import Cart from './Pages/Cart';
+//import Cart from './Pages/Cart';
 import FullPizza from './Pages/FullPizza';
 
 
 import { Route, Routes } from "react-router-dom";
 import MainLayoot from './Pages/layouts/MainLayout';
-
+const Cart = React.lazy(()=>import('./Pages/Cart'));
 
 function App() {
   return (
@@ -16,7 +16,10 @@ function App() {
       <Route path="/" element={<MainLayoot />}>
         <Route path="/" element={<Home />} />
         <Route path="/not-found" element={<NotFound />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={
+        <React.Suspense fallback={<div>Идет загрузка...</div>}>
+          <Cart />
+          </React.Suspense>} />
         <Route path="/pizza/:id" element={<FullPizza />} />
         <Route path="*" element={<NotFound />} />
       </Route>
